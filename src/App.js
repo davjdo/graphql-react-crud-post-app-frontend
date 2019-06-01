60ad0ae8e0d5e81946d5f0ee5a044aff604f6cf1
davjdo <davido_204@hotmail.com>
2019-06-01 21:42:12 +0200

    add Toolbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component, Fragment } from 'react';
 
 import Layout from './components/Layout/Layout';
+import Toolbar from './components/Toolbar/Toolbar';
 class App extends Component {
 	render() {
 		return (
 			<Fragment>
-				<Layout />
+				<Layout header={<Toolbar />} />
 			</Fragment>
 		);
 	}
