911d029e7decbebaba2ffa5235a293f4e3cbfaaa
davjdo <davido_204@hotmail.com>
2019-06-02 21:05:47 +0200

    add signup feature with SignupPage, Auth, Input, Button and validators components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component, Fragment } from 'react';
+import { Route, Switch, withRouter } from 'react-router-dom';
 
 import Backdrop from './components/Backdrop/Backdrop';
 import Layout from './components/Layout/Layout';
 import Toolbar from './components/Toolbar/Toolbar';
 import MainNavigation from './components/Navigation/MainNavigation/MainNavigation';
 import MobileNavigation from './components/Navigation/MobileNavigation/MobileNavigation';
+import SignupPage from './pages/Auth/Signup';
 
 class App extends Component {
 	state = {
 		showBackdrop: false,
-		showMobileNav: false
+		showMobileNav: false,
+		isAuth: false,
+		authLoading: false,
+		error: null
 	};
 
 	backdropClickHandler = () => {
@@ -20,7 +25,73 @@ class App extends Component {
 		this.setState({ showMobileNav: isOpen, showBackdrop: isOpen });
 	};
 
+	signupHandler = (event, authData) => {
+		event.preventDefault();
+		this.setState({ authLoading: true });
+		const graphqlQuery = {
+			query: `
+        mutation CreateUser($email: String!, $name: String!, $password: String!) {
+          createUser(userInput: {email: $email, name: $name, password: $password}) {
+            _id
+            email
+          }
+       	 }
+			`,
+			variables: {
+				email: authData.signupForm.email.value,
+				name: authData.signupForm.name.value,
+				password: authData.signupForm.password.value
+			}
+		};
+		fetch('http://localhost:3002/graphql', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(graphqlQuery)
+		})
+			.then(res => {
+				return res.json();
+			})
+			.then(resData => {
+				if (resData.errors && resData.errors[0].status === 422) {
+					throw new Error(
+						"Validation failed. Make sure the email address isn't used yet!"
+					);
+				}
+				if (resData.errors) {
+					throw new Error('User creation failed!');
+				}
+				console.log(resData);
+				this.setState({ isAuth: false, authLoading: false });
+				this.props.history.replace('/');
+			})
+			.catch(err => {
+				console.log(err);
+				this.setState({
+					isAuth: false,
+					authLoading: false,
+					error: err
+				});
+			});
+	};
+
 	render() {
+		let routes = (
+			<Switch>
+				<Route
+					path="/signup"
+					exact
+					render={props => (
+						<SignupPage
+							{...props}
+							onSignup={this.signupHandler}
+							loading={this.state.authLoading}
+						/>
+					)}
+				/>
+			</Switch>
+		);
 		return (
 			<Fragment>
 				{this.state.showBackdrop && (
@@ -42,9 +113,10 @@ class App extends Component {
 						/>
 					}
 				/>
+				{routes}
 			</Fragment>
 		);
 	}
 }
 
-export default App;
+export default withRouter(App);
