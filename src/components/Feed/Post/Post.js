3a65184cb2019b67100e8e1be0ee51e1a9cc5ac5
davjdo <davido_204@hotmail.com>
2019-06-09 23:08:52 +0200

    fetching posts feature on FeedPage with Post and Loader components

---
+++ src/components/Feed/Post/Post.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Button from '../../Button/Button';
+
+import './Post.css';
+
+const post = props => (
+	<article className="post">
+		<header className="post__header">
+			<h3 className="post__meta">
+				Posted by {props.author} on {props.date}
+			</h3>
+			<h1 className="post__title">{props.title}</h1>
+		</header>
+		{/* <div className="post__image">
+      <Image imageUrl={props.image} contain />
+    </div>
+    <div className="post__content">{props.content}</div> 
+    */}
+		<div className="post__actions">
+			<Button mode="flat" link={props.id}>
+				View
+			</Button>
+			<Button mode="flat">Edit</Button>
+			<Button mode="flat" design="danger">
+				Delete
+			</Button>
+		</div>
+	</article>
+);
+
+export default post;
--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import ErrorHandler from '../../components/ErrorHandler/ErrorHandler';
+import Loader from '../../components/Loader/Loader';
 import Button from '../../components/Button/Button';
+import Post from '../../components/Feed/Post/Post';
 import FeedEdit from '../../components/Feed/FeedEdit/FeedEdit';
 
 import './Feed.css';
@@ -10,7 +12,68 @@ class Feed extends Component {
 		isEditing: false,
 		editLoading: false,
 		posts: [],
+		postPage: 1,
+		totalPosts: 0,
+		postsLoading: true, // on start, loading post
 		error: null
+	};
+
+	componentDidMount() {
+		this.loadPosts();
+	}
+
+	loadPosts = () => {
+		this.setState({ postsLoading: true });
+		let page = this.state.postPage;
+		const graphqlQuery = {
+			query: `
+				query FetchPosts($page: Int!) {
+					posts(page: $page) {
+						posts {
+							_id
+							title
+							content
+							imageUrl
+							creator {
+								name
+							}
+							createdAt
+						}
+						totalPosts
+					}
+				}
+			`,
+			variables: {
+				page: page
+			}
+		};
+		fetch('http://localhost:3002/graphql', {
+			method: 'POST',
+			headers: {
+				Authorization: 'Bearer ' + this.props.token,
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(graphqlQuery)
+		})
+			.then(res => {
+				return res.json();
+			})
+			.then(resData => {
+				if (resData.errors) {
+					throw new Error('Fetching posts failed');
+				}
+				this.setState({
+					posts: resData.data.posts.posts.map(post => {
+						return {
+							...post,
+							imagePath: post.imageUrl
+						};
+					}),
+					totalPosts: resData.data.posts.totalPosts,
+					postsLoading: false
+				});
+			})
+			.catch(this.catchError);
 	};
 
 	newPostHandler = () => {
@@ -60,7 +123,9 @@ class Feed extends Component {
 						'Content-Type': 'application/json'
 					}
 				})
-					.then(res => res.json())
+					.then(res => {
+						return res.json();
+					})
 					.then(resData => {
 						if (resData.errors && resData.errors[0].status === 422) {
 							throw new Error(
@@ -107,6 +172,10 @@ class Feed extends Component {
 		this.setState({ error: null });
 	};
 
+	catchError = error => {
+		this.setState({ error: error });
+	};
+
 	render() {
 		return (
 			<Fragment>
@@ -121,6 +190,31 @@ class Feed extends Component {
 					<Button mode="raised" design="accent" onClick={this.newPostHandler}>
 						New Post
 					</Button>
+				</section>
+				<section className="feed">
+					{this.state.postsLoading && (
+						<div style={{ textAlign: 'center', marginTop: '2rem' }}>
+							<Loader />
+						</div>
+					)}
+					{this.state.posts.length <= 0 && !this.state.postsLoading ? (
+						<p style={{ textAlign: 'center' }}>No posts found.</p>
+					) : null}
+					{!this.state.postsLoading && (
+						<div>
+							{this.state.posts.map(post => (
+								<Post
+									key={post._id}
+									id={post._id}
+									author={post.creator.name}
+									date={new Date(post.createdAt).toLocaleDateString('en-US')}
+									title={post.title}
+									image={post.imageUrl}
+									content={post.content}
+								/>
+							))}
+						</div>
+					)}
 				</section>
 			</Fragment>
 		);
